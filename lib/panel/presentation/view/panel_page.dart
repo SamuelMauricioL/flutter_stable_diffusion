1caa8848b931f17f2651f851ad47d3e5623989f5
Samuel Mauricio Laime <samuelmauriciolaime@gmail.com>
2022-08-28 14:41:08 -0500

    chore: add Panel layout

--- lib/panel/presentation/view/panel_page.dart
+++ lib/panel/presentation/view/panel_page.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class PanelPage extends StatelessWidget {
+class PanelPage extends StatefulWidget {
   const PanelPage({super.key});
+
+  @override
+  State<PanelPage> createState() => _PanelPageState();
+}
+
+class _PanelPageState extends State<PanelPage> {
+  final _dreamController = TextEditingController();
+
+  @override
+  void dispose() {
+    _dreamController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +22,36 @@ class PanelPage extends StatelessWidget {
       appBar: AppBar(
         title: const Center(child: Text('Panel')),
       ),
-      body: Column(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24),
+          child: Column(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+                child: TextField(
+                  controller: _dreamController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter a search term',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 80),
+              Row(
+                children: [
+                  Expanded(
+                    child: Image.network(
+                      'https://picsum.photos/250?image=9',
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
